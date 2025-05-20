fbd40a2357c2a5925b1c7f15dbd40329937ffdb6
Didzis Avotins <didzis.avotins@gmail.com>
2025-05-20 12:39:56 +0300

    Page header; Mobile menu

--- web/themes/custom/satport_theme/js/main.js
+++ web/themes/custom/satport_theme/js/main.js
@@ -1,5 +1,33 @@
 (function ($, Drupal, once) {
   Drupal.behaviors.satport_theme = {
-    attach: function (context, settings) {},
+    attach: function (context, settings) {
+      const pageHeader = document.getElementById("page-header");
+      const mainMenuFixed = document.getElementById("mobile-menu-fixed");
+      const mainMenuScroll = document.getElementById("mobile-menu-scroll");
+      const mobileMenuToggle = document.getElementById("mobile-menu-toggle");
+
+      mobileMenuToggle.addEventListener("click", () => {
+        mainMenuFixed.classList.toggle("open");
+        mobileMenuToggle.classList.toggle("open");
+        document.documentElement.classList.toggle("scroll-lock");
+      });
+
+      function onResize() {
+        // Set Mobile menu container to fixed height to enable scrolling when necessary
+        mainMenuScroll.style.height =
+          window.innerHeight - pageHeader.clientHeight + "px";
+
+        // Reset Mobile menu visibility when resized to desktop
+        if (window.innerWidth >= 1024) {
+          mainMenuFixed.classList.remove("open");
+          mobileMenuToggle.classList.remove("open");
+          document.documentElement.classList.remove("scroll-lock");
+        }
+      }
+
+      // Attach the event listener
+      window.addEventListener("resize", onResize);
+      onResize();
+    },
   };
 })(jQuery, Drupal, once);
