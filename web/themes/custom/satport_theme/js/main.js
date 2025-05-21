957db382cfc4df1f06a0fa9c2f7fdc56414fdd9b
Didzis Avotins <didzis.avotins@gmail.com>
2025-05-21 12:45:12 +0300

    Section: Full width image

--- web/themes/custom/satport_theme/js/main.js
+++ web/themes/custom/satport_theme/js/main.js
@@ -6,6 +6,9 @@
       const mainMenuScroll = document.getElementById("mobile-menu-scroll");
       const mobileMenuToggle = document.getElementById("mobile-menu-toggle");
       const heroSections = document.querySelectorAll(".paragraph--type--hero");
+      const fullWidthImageSections = document.querySelectorAll(
+        ".paragraph--type--full-width-image"
+      );
 
       mobileMenuToggle.addEventListener("click", () => {
         mainMenuFixed.classList.toggle("open");
@@ -14,18 +17,18 @@
       });
 
       function onResize() {
-        // Set Mobile menu container to fixed height to enable scrolling when necessary
+        // Set Mobile menu container to fixed height to enable scrolling when necessary.
         mainMenuScroll.style.height =
           window.innerHeight - pageHeader.clientHeight + "px";
 
-        // Reset Mobile menu visibility when resized to desktop
+        // Reset Mobile menu visibility when resized to desktop.
         if (window.innerWidth >= 1024) {
           mainMenuFixed.classList.remove("open");
           mobileMenuToggle.classList.remove("open");
           document.documentElement.classList.remove("scroll-lock");
         }
 
-        // Hero: Set min-height to be window height
+        // Hero: Set min-height to be window height.
         heroSections.forEach(function (heroSection) {
           if (window.innerWidth >= 1024) {
             heroSection.style.height = window.innerHeight + "px";
@@ -34,9 +37,19 @@
               window.innerHeight - pageHeader.clientHeight + "px";
           }
         });
+
+        // Section: Full Width Image.
+        // Apply correct background image (mobile/desktop).
+        fullWidthImageSections.forEach(function (fullWidthImageSection) {
+          if (window.innerWidth >= 1024) {
+            fullWidthImageSection.style.backgroundImage = `url(${fullWidthImageSection.dataset.desktopImage})`;
+          } else {
+            fullWidthImageSection.style.backgroundImage = `url(${fullWidthImageSection.dataset.mobileImage})`;
+          }
+        });
       }
 
-      // Attach the event listener
+      // Attach the event listener.
       window.addEventListener("resize", onResize);
       onResize();
     },
