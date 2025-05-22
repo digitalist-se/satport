4f1f076ba95b8745ff90a62256908d3506687ef4
Didzis Avotins <didzis.avotins@gmail.com>
2025-05-22 13:48:33 +0300

    Desktop menu styles

--- web/themes/custom/satport_theme/js/main.js
+++ web/themes/custom/satport_theme/js/main.js
@@ -9,13 +9,44 @@
       const fullWidthImageSections = document.querySelectorAll(
         ".paragraph--type--full-width-image"
       );
+      const desktopMenuSticky = document.getElementById("desktop-menu-sticky");
+      const desktopMenu = document.getElementById("desktop-menu");
+      const tagSections = document.querySelectorAll(".tag-section");
+      const desktopMenuLinks = document.querySelectorAll("#desktop-menu a");
 
+      // Mobile Menu toggle
       mobileMenuToggle.addEventListener("click", () => {
         mainMenuFixed.classList.toggle("open");
         mobileMenuToggle.classList.toggle("open");
         document.documentElement.classList.toggle("scroll-lock");
       });
 
+      // Render Desktop and Mobile menu links from tag sections.
+      tagSections.forEach(function (tagSection) {});
+
+      // Desktop menu position: middle of first section with title (data-section-title) attribute.
+      function setDesktopMenuPosition() {
+        const firstTag = document.querySelector(".section [data-tag]");
+        const topOffset = firstTag.getBoundingClientRect().top + window.scrollY;
+        desktopMenuSticky.style.paddingTop = topOffset + "px";
+      }
+
+      //
+      function getTagSectionPositions() {
+        let values = [];
+        tagSections.forEach(function (tagSection) {
+          const topOffset =
+            tagSection.getBoundingClientRect().top + window.scrollY;
+          const bottomOffset =
+            tagSection.getBoundingClientRect().bottom + window.scrollY;
+
+          values.push({ topOffset, bottomOffset });
+        });
+
+        return values;
+      }
+
+      let tagSectionPositions = [];
       function onResize() {
         // Set Mobile menu container to fixed height to enable scrolling when necessary.
         mainMenuScroll.style.height =
@@ -47,11 +78,71 @@
             fullWidthImageSection.style.backgroundImage = `url(${fullWidthImageSection.dataset.mobileImage})`;
           }
         });
+
+        //
+        if (window.innerWidth >= 1024) {
+          setDesktopMenuPosition();
+          tagSectionPositions = getTagSectionPositions();
+        }
       }
 
       // Attach the event listener.
       window.addEventListener("resize", onResize);
       onResize();
+
+      function setDesktopMenuActiveItem() {
+        if (!tagSectionPositions.length) return;
+
+        const desktopMenuTopOffset =
+          desktopMenu.getBoundingClientRect().top + window.scrollY;
+        let activeIndex;
+        let i = 0;
+        for (const tagSectionPosition of tagSectionPositions) {
+          if (
+            desktopMenuTopOffset >= tagSectionPosition.topOffset &&
+            desktopMenuTopOffset <= tagSectionPosition.bottomOffset
+          ) {
+            activeIndex = i;
+            break;
+          }
+          i++;
+        }
+
+        // Set active.
+        i = 0;
+        for (const desktopMenuLink of desktopMenuLinks) {
+          desktopMenuLink.classList.remove("active");
+
+          if (i === activeIndex) {
+            desktopMenuLink.classList.add("active");
+          }
+
+          i++;
+        }
+      }
+
+      // Attach the scroll listener.
+      let lastKnownScrollPosition = 0;
+      let ticking = false;
+      function onScroll(scrollPos) {
+        // console.log(scrollPos);
+        // Set active Desktop Menu item
+        setDesktopMenuActiveItem();
+      }
+
+      document.addEventListener("scroll", (event) => {
+        lastKnownScrollPosition = window.scrollY;
+
+        if (!ticking) {
+          window.requestAnimationFrame(() => {
+            onScroll(lastKnownScrollPosition);
+            ticking = false;
+          });
+
+          ticking = true;
+        }
+      });
+      onScroll(0);
     },
   };
 })(jQuery, Drupal, once);
